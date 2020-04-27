fce70aabdde1613a2892378bd7c2b4c56bd2b30f
KDSGYT <kdsgyt@gmail.com>
2020-04-27 05:27:31 -0400

    Decided to use nav as the component that will be defined inside the app.component.html,instead of generating a seperate component. This will save complexicity of the web app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//
+import { MatToolbarModule } from '@angular/material/toolbar'
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// export class MatToolbarModule { }
